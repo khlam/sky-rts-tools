4c5a53be2250d9d76275c54e2c21d1604f83215e
khlam <4841220+khlam@users.noreply.github.com>
2018-02-13 16:43:47 -0800

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Part A, Build SCAII core and move glues
 cd SCAII/
-git checkout dev
+#git checkout dev
 cargo build --release
 rm -rf ~/.scaii/
 
@@ -21,7 +21,7 @@ mkdir ~/.scaii/glue/python/scaii/env/sky_rts
 
 # Part B, Build  Sky-RTS 
 cd ../Sky-RTS/
-git checkout dev
+#git checkout dev
 cd backend/
 cargo build --release
 cp target/release/libbackend.so ~/.scaii/backends/bin/libsky-rts.so
@@ -29,3 +29,6 @@ cd ..
 cp -r game_wrapper/python/* ~/.scaii/glue/python/scaii/env/sky_rts/
 
 cp backend/lua/tower_example.lua ~/.scaii/backends/sky-rts/maps
+
+export PYTHONPATH=$PYTHONPATH:/home/lamki/.scaii/bin:/home/lamki/.scaii/glue/python/ 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lamki/.scaii/bin/
